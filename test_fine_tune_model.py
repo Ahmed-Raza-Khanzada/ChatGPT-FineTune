aa3791f9163132855542a8908499105eadfb1e98
Ahmed Khanzada <ahmedrazakhanzada@hotmail.com>
2023-11-06 11:00:55 +0500

    updated api file

--- test_fine_tune_model.py
+++ test_fine_tune_model.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from api_credentials import api_key,model_id
 
 app = Flask(__name__)
-openai.api_key = api_key  # Be cautious with your API keys!
+openai.api_key = api_key  
 
 def clean_html(message):
     soup = BeautifulSoup(message, 'html.parser')
@@ -13,23 +13,36 @@ def clean_html(message):
 
 def chat_with_fine_tuned_model(messages):
     response = openai.ChatCompletion.create(
-        model=model_id,  # Replace with your fine-tuned model ID
+        model=model_id,  
         messages=messages
     )
     reply = response['choices'][0]['message']['content']
     return reply
-
+def check_null(var):
+    if var == None:
+        return "NA"
+    elif type(var)==int or type(var)==float:
+        return str(var)
+    return var
 @app.route('/chat', methods=['POST'])
 def chat():
     try:
         # Extract message from POST request
         data = request.json
         message = clean_html(data["message"])
+        model = data["model"]
+        manufacturer = data["manufacturer"]
+        year_of_manufactured = data["year_of_manufactured"]
+        ecu_number = data["ecu_number"]
+        model = check_null(model)
+        manufacturer= check_null(manufacturer)
+        ecu_number = check_null(ecu_number)
+        year_of_manufactured = check_null(year_of_manufactured)
         message = " ".join(message.split("\u00a0"))
 
         # Prepare messages for ChatGPT
         messages = [
-            {"role": "system", "content": "You are an assistant from staff that responds to user queries:"},
+            {"role": "system", "content": f"You are an assistant from staff that responds to Clients queries\nhere are their vehicle details:\nModel:{model}\nManufacturer:{manufacturer}\nYear of Manufature:{year_of_manufactured}\nECU Number:{ecu_number}\n"},
             {"role": "user", "content": message}
         ]
 
--- train_GPT.py
+++ train_GPT.py
@@ -1,6 +1,8 @@
 # import openai
 import json
 from bs4 import BeautifulSoup
+import time
+import random
 # Set your OpenAI API token
 # api_key = "YOUR_API_KEY"
 # openai.api_key = api_key
@@ -8,8 +10,9 @@ from bs4 import BeautifulSoup
 # Sample data
 
 
-data = json.load(open("Final_Ticket_messages.json"))
-
+# data = json.load(open("Final_Ticket_messages.json"))
+start_time =time.time() 
+data = json.load(open("Final_Ticket_messages_LeftJOin.json"))
 def clean_html(message):
     soup = BeautifulSoup(message, 'html.parser')
     text = soup.get_text()
@@ -22,103 +25,115 @@ for ticket_id in data:
     unique_ticket_ids.add(ticket_id["Thread ID"])
 
 # Convert the set back to a list if needed
-unique_ticket_ids_list = list(unique_ticket_ids)
-train_part = int(0.9 * len(unique_ticket_ids_list))
+totalunique_ticket_ids_list = list(unique_ticket_ids)
 
-train_data = unique_ticket_ids_list[:train_part]
-val_data = unique_ticket_ids_list[train_part:]
-def check_message(current_conversation):
-    assistant = False
-    user = False
-    local_words = 0
-    for h in current_conversation:
-        if h["role"] == "assistant":
-            assistant =True
-        if h["role"] == "user":
-            user = True
-        local_words += len(h["content"].split(" "))
+totalunique_ticket_ids_list = sorted(totalunique_ticket_ids_list, key=lambda x: random.random())
+print(totalunique_ticket_ids_list)
+print("Total No of Chats : ",len(totalunique_ticket_ids_list))
+print("Dividing chats in to 12 parts")
+unique_ticket_ids_list_part = len(totalunique_ticket_ids_list)//12
+for i in range(12):
+    print("*"*40,i,"*"*40)
+    unique_ticket_ids_list = totalunique_ticket_ids_list[i*unique_ticket_ids_list_part:(i+1)*unique_ticket_ids_list_part]
+    print(f"Total {i} file length is {len(unique_ticket_ids_list)}")
+    train_part = int(0.9 * len(unique_ticket_ids_list))
+    train_data = unique_ticket_ids_list[:train_part]
+    val_data = unique_ticket_ids_list[train_part:]
+    print("Train Data Length: ",len(train_data))
+    print("Validation Data Length: ",len(val_data))
+    def check_message(current_conversation):
+        assistant = False
+        user = False
+        local_words = 0
+        for h in current_conversation:
+            if h["role"] == "assistant":
+                assistant =True
+            if h["role"] == "user":
+                user = True
+            local_words += len(h["content"].split(" "))
+            if assistant and user:
+                break
         if assistant and user:
-            break
-    if assistant and user:
-        return True,local_words
-    return False, local_words
-def get_data(data, data1,chat_limit = 100000):
-    training_data = []
-    current_conversation = []
-    no_of_tokens = 0
-    current_ticket_id = None
-    def check_var(var):
-        if var==None:
-            return "NA"
-        return var.strip()
-    for message in data:
-        ticket_id = message["Thread ID"]
-        if ticket_id not in data1:
-             continue
-        if len(training_data) >= chat_limit:
-            break
-        if ticket_id != current_ticket_id:
-            if current_conversation:
-                flag,local_tokens = check_message(current_conversation)
-                if flag:
-                    no_of_tokens += local_tokens
-                    training_data.append({"messages": current_conversation})
-                else:
-                    
-                    current_conversation = []
-                    continue
-            current_conversation = []
-            model,manufacturer,year_of_manufactured,ecu_number = message["Model"],message["Manufacturer"],message["Year of Manufacture"],message["ECU Number"]
-            model,manufacturer,year_of_manufactured,ecu_number = check_var(model),check_var(manufacturer),check_var(year_of_manufactured),check_var(ecu_number)
-            current_conversation.append({"role": "system", "content": f"You are an assistant from staff that responds to Clients queries\nhere are their vehicle details:\nModel:{model}\nManufacturer:{manufacturer}\nYear of Manufature:{year_of_manufactured}\nECU Number:{ecu_number}\n"})
-            current_ticket_id = ticket_id
+            return True,local_words
+        return False, local_words
+    def get_data(data, data1,chat_limit = 100000):
+        training_data = []
+        current_conversation = []
+        no_of_tokens = 0
+        current_ticket_id = None
+        def check_var(var):
+            if var==None:
+                return "NA"
+            return var.strip()
+        for message in data:
+            ticket_id = message["Thread ID"]
+            if ticket_id not in data1:
+                continue
+            if len(training_data) >= chat_limit:
+                break
+            if ticket_id != current_ticket_id:
+                if current_conversation:
+                    flag,local_tokens = check_message(current_conversation)
+                    if flag:
+                        no_of_tokens += local_tokens
+                        training_data.append({"messages": current_conversation})
+                    else:
+                        
+                        current_conversation = []
+                        continue
+                current_conversation = []
+                model,manufacturer,year_of_manufactured,ecu_number = message["Model"],message["Manufacturer"],message["Year of Manufacture"],message["ECU Number"]
+                model,manufacturer,year_of_manufactured,ecu_number = check_var(model),check_var(manufacturer),check_var(year_of_manufactured),check_var(ecu_number)
+                current_conversation.append({"role": "system", "content": f"You are an assistant from staff that responds to Clients queries\nhere are their vehicle details:\nModel:{model}\nManufacturer:{manufacturer}\nYear of Manufature:{year_of_manufactured}\nECU Number:{ecu_number}\n"})
+                current_ticket_id = ticket_id
 
-        role = "user" if message["Message Type"] == "User Message" else "assistant"
-        message = " ".join(clean_html(message["Message Content"]).split("\u00a0"))
-        current_conversation.append({"role": role, "content": message})
+            role = "user" if message["Message Type"] == "User Message" else "assistant"
+            message = " ".join(clean_html(message["Message Content"]).split("\u00a0"))
+            current_conversation.append({"role": role, "content": message})
 
-    if current_conversation:
-        flag ,local_tokens= check_message(current_conversation)
-        if flag:
-            no_of_tokens += local_tokens
-            training_data.append({"messages": current_conversation})
-        else:
-          
-            current_conversation = []
-    print("No_Of_Tokens: ", no_of_tokens)
-    return training_data
-train_smaple_size = 1000
-val_sample_size = 100
-if train_smaple_size > len(train_data):
+        if current_conversation:
+            flag ,local_tokens= check_message(current_conversation)
+            if flag:
+                no_of_tokens += local_tokens
+                training_data.append({"messages": current_conversation})
+            else:
+            
+                current_conversation = []
+        print("No_Of_Tokens: ", no_of_tokens)
+        return training_data
     train_smaple_size = len(train_data)
-if val_sample_size > len(val_data):
     val_sample_size = len(val_data)
-print("*"*20,"Training Data ","*"*20)
-training_data = get_data(data, train_data,chat_limit=train_smaple_size)
-print("Training Data goted",)
-print("Creating file for Training Data")
-# Write training data to a JSONL file
-with open(f'training_data_{train_smaple_size}.jsonl', 'w') as jsonl_file:
-    for entry in training_data:
-        jsonl_file.write(json.dumps(entry) + '\n')
+    if train_smaple_size > len(train_data):
+        train_smaple_size = len(train_data)
+    if val_sample_size > len(val_data):
+        val_sample_size = len(val_data)
+    print("*"*20,"Training Data ","*"*20)
+    training_data = get_data(data, train_data,chat_limit=train_smaple_size)
+    print("Training Data goted",)
+    print("Creating file for Training Data")
+    # Write training data to a JSONL file
+    with open(f'training_data_{len(training_data)}_{i}.jsonl', 'w') as jsonl_file:
+        for entry in training_data:
+            jsonl_file.write(json.dumps(entry) + '\n')
 
 
-print("Training Data File Created")
-print("*"*20,"Validation Data","*"*20)
-print("Creating file for Validation Data")
-validation_data = get_data(data, val_data,chat_limit=val_sample_size)
-print("Validation Data goted",)
-print("Creating file for Validation Data")
-# Write validation data to a JSONL file
-with open(f'validation_data_{val_sample_size}.jsonl', 'w') as jsonl_file:
-    for entry in validation_data:
-        jsonl_file.write(json.dumps(entry) + '\n')
-print("Validation Data File Created")
-print(len(training_data), len(validation_data))
-print("Done Creating JSONL Files")
-
-
-
+    print("Training Data File Created")
+    print("*"*20,"Validation Data","*"*20)
+    print("Creating file for Validation Data")
+    validation_data = get_data(data, val_data,chat_limit=val_sample_size)
+    print("Validation Data goted",)
+    print("Creating file for Validation Data")
+    # Write validation data to a JSONL file
+    with open(f'validation_data_{len(validation_data)}_{i}.jsonl', 'w') as jsonl_file:
+        for entry in validation_data:
+            jsonl_file.write(json.dumps(entry) + '\n')
+    end_time  = time.time()
+    time1 = end_time - start_time
+    #time in minutes
+    print("Time Taken: ", time1/60)
+    print("Validation Data File Created")
+    print(len(training_data), len(validation_data))
+    print("Done Creating JSONL Files")
 # # Train the model
 # response = openai.ChatCompletion.create(
 #     model="gpt-3.5-turbo",
@@ -127,3 +142,5 @@ print("Done Creating JSONL Files")
 
 # # Print the model's reply
 # print(response['choices'][0]['message']['content'])
+
+
